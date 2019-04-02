ee2c6433e983885916c253d04ea5297467cbedb3
Peyton Ratliff <ratliffbox@gmail.com>
2019-04-01 20:19:47 -0400

    Peyton / AttendDoW
    Added the AttendDoW form

---
+++ dbMaintenanceBound/dbMaintenanceBound/AttendDoW.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace dbMaintenanceBound
+{
+    partial class AttendDoW
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AttendDoW));
+            this.attendanceDataSet = new dbMaintenanceBound.AttendanceDataSet();
+            this.attendDoWBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.attendDoWTableAdapter = new dbMaintenanceBound.AttendanceDataSetTableAdapters.attendDoWTableAdapter();
+            this.tableAdapterManager = new dbMaintenanceBound.AttendanceDataSetTableAdapters.TableAdapterManager();
+            this.attendDoWBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.attendDoWBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.attendDoWDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.attendanceDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWBindingNavigator)).BeginInit();
+            this.attendDoWBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // attendanceDataSet
+            // 
+            this.attendanceDataSet.DataSetName = "AttendanceDataSet";
+            this.attendanceDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // attendDoWBindingSource
+            // 
+            this.attendDoWBindingSource.DataMember = "attendDoW";
+            this.attendDoWBindingSource.DataSource = this.attendanceDataSet;
+            // 
+            // attendDoWTableAdapter
+            // 
+            this.attendDoWTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.activityLogTableAdapter = null;
+            this.tableAdapterManager.attendBlackOutTypesTableAdapter = null;
+            this.tableAdapterManager.attendDoWTableAdapter = this.attendDoWTableAdapter;
+            this.tableAdapterManager.attendLogTypesTableAdapter = null;
+            this.tableAdapterManager.attendRolesTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.blackOutDatesTableAdapter = null;
+            this.tableAdapterManager.courseInfoTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = dbMaintenanceBound.AttendanceDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.UsersTableAdapter = null;
+            // 
+            // attendDoWBindingNavigator
+            // 
+            this.attendDoWBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.attendDoWBindingNavigator.BindingSource = this.attendDoWBindingSource;
+            this.attendDoWBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.attendDoWBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.attendDoWBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.attendDoWBindingNavigatorSaveItem});
+            this.attendDoWBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.attendDoWBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.attendDoWBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.attendDoWBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.attendDoWBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.attendDoWBindingNavigator.Name = "attendDoWBindingNavigator";
+            this.attendDoWBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.attendDoWBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.attendDoWBindingNavigator.TabIndex = 0;
+            this.attendDoWBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // attendDoWBindingNavigatorSaveItem
+            // 
+            this.attendDoWBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.attendDoWBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("attendDoWBindingNavigatorSaveItem.Image")));
+            this.attendDoWBindingNavigatorSaveItem.Name = "attendDoWBindingNavigatorSaveItem";
+            this.attendDoWBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.attendDoWBindingNavigatorSaveItem.Text = "Save Data";
+            this.attendDoWBindingNavigatorSaveItem.Click += new System.EventHandler(this.attendDoWBindingNavigatorSaveItem_Click);
+            // 
+            // attendDoWDataGridView
+            // 
+            this.attendDoWDataGridView.AutoGenerateColumns = false;
+            this.attendDoWDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.attendDoWDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.attendDoWDataGridView.DataSource = this.attendDoWBindingSource;
+            this.attendDoWDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.attendDoWDataGridView.Location = new System.Drawing.Point(0, 25);
+            this.attendDoWDataGridView.Name = "attendDoWDataGridView";
+            this.attendDoWDataGridView.Size = new System.Drawing.Size(800, 437);
+            this.attendDoWDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "dayOfWeek";
+            this.dataGridViewTextBoxColumn2.HeaderText = "dayOfWeek";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // AttendDoW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.attendDoWDataGridView);
+            this.Controls.Add(this.attendDoWBindingNavigator);
+            this.Name = "AttendDoW";
+            this.Text = "AttendDoW";
+            this.Load += new System.EventHandler(this.AttendDoW_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.attendanceDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWBindingNavigator)).EndInit();
+            this.attendDoWBindingNavigator.ResumeLayout(false);
+            this.attendDoWBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.attendDoWDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AttendanceDataSet attendanceDataSet;
+        private System.Windows.Forms.BindingSource attendDoWBindingSource;
+        private AttendanceDataSetTableAdapters.attendDoWTableAdapter attendDoWTableAdapter;
+        private AttendanceDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator attendDoWBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton attendDoWBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView attendDoWDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
---
+++ dbMaintenanceBound/dbMaintenanceBound/AttendDoW.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbMaintenanceBound
+{
+    public partial class AttendDoW : Form
+    {
+        public AttendDoW()
+        {
+            InitializeComponent();
+        }
+
+        private void attendDoWBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.attendDoWBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.attendanceDataSet);
+
+        }
+
+        private void AttendDoW_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'attendanceDataSet.attendDoW' table. You can move, or remove it, as needed.
+            this.attendDoWTableAdapter.Fill(this.attendanceDataSet.attendDoW);
+
+        }
+    }
+}
